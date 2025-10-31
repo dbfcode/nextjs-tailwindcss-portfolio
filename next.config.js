74c8fcb7a28739788f01b23f2a1f4fba5d6f3a5a
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-10-31 20:24:46 -0300

    Update i18n configuration in next.config.js to specify locales and default settings
    - Defined supported locales as Portuguese (pt-BR) and English (en).
    - Set default locale to Portuguese (pt-BR) and enabled locale detection for improved user experience.

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   reactStrictMode: true,
-  i18n,
+  i18n: {
+    locales: ["pt-BR", "en"],
+    defaultLocale: "pt-BR",
+    localeDetection: true,
+  },
 };
