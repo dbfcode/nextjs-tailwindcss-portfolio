7130f2f3eaf781f544151860d9bc98bf278c7a8b
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-15 06:54:33 -0300

    Add PlayButton component for enhanced user interaction
    - Introduced a new PlayButton component to facilitate project engagement.
    - The button includes a customizable label and click handler, with styling for improved user experience.
    - Integrated Play icon from lucide-react for visual appeal.

---
+++ components/reusable/PlayButton.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Play } from "lucide-react";
+
+export function PlayButton({
+  label = "Confira o projeto",
+  onClick,
+  className = "",
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex items-center gap-2 text-md font-general-medium min-w-24 bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300 mb-4 ${className}`}
+      aria-label={label}
+    >
+      <Play size={18} />
+      {label}
+    </button>
+  );
+}
