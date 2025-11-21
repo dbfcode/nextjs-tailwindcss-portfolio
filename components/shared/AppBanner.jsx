c5a62593f2a1300456a200c8ad7f29c7926feb30
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-21 14:41:12 -0300

    styles: atualizando estilo de botão de download curriculum

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -51,11 +51,16 @@ function AppBanner() {
           <a
             download="DIÊGO_CURRICULUM.pdf"
             href="/files/DIÊGO_CURRICULUM.pdf"
-            className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+            className="font-general-medium inline-flex items-center mt-12 mb-6 sm:mb-0 text-lg
+             border border-indigo-200 dark:border-ternary-dark p-2 shadow-lg rounded-lg
+             bg-indigo-50 hover:bg-indigo-500 text-gray-500 hover:text-white
+             focus:ring-1 focus:ring-indigo-900 duration-500"
             aria-label="Download Resume"
           >
-            <FiArrowDownCircle className="ml-0 sm:ml-1 mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
-            <span className="text-sm sm:text-lg duration-100">Download CV</span>
+            <FiArrowDownCircle className="ml-0 sm:ml-1 mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-70 "></FiArrowDownCircle>
+            <span className="text-sm sm:text-lg duration-70 ">
+              Baixar Curriculum Vitae
+            </span>
           </a>
         </motion.div>
       </div>
